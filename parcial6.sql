7215cb0955fbad5660d9bffd7174491c87bc408e
Camila Gil <camilabgil@hotmail.com>
2017-06-23 23:40:44 -0300

    Update parcial6.sql

--- parcial6.sql
+++ parcial6.sql
@@ -31,3 +31,31 @@ FROM DEPOSITO D
 	JOIN STOCK S ON S.stoc_deposito = D.depo_codigo
 	JOIN PRODUCTO P ON P.prod_codigo = S.stoc_producto
 	JOIN RUBRO R ON P.prod_rubro = R.rubr_id
+	
+	
+--2--
+
+SELECT DO.DEPO_CODIGO,
+	(COUNT(E.empl_codigo)) AS 'cantEmpleados',
+	AVG(DATEDIFF(YEAR,E.empl_nacimiento,GETDATE())) AS 'edadPromedio',
+	(SELECT TOP 1 E2.empl_apellido + E2.empl_nombre
+		FROM EMPLEADO E2
+			JOIN DEPARTAMENTO D2 ON D2.depa_codigo = E2.empl_departamento
+			JOIN DEPOSITO D3 ON D3.depo_zona = D2.depa_zona
+		WHERE D3.depo_codigo = DO.depo_codigo
+		ORDER BY E2.empl_nacimiento ASC) 'masViejo',
+	(CASE WHEN (SELECT COUNT(*)
+			FROM EMPLEADO E 
+			WHERE E.empl_jefe = (SELECT TOP 1 E2.empl_codigo
+									FROM EMPLEADO E2
+										JOIN DEPARTAMENTO D2 ON D2.depa_codigo = E2.empl_departamento
+										JOIN DEPOSITO D3 ON D3.depo_zona = D2.depa_zona
+									WHERE D3.depo_codigo = DO.depo_codigo
+									ORDER BY E2.empl_nacimiento ASC)) > 0
+		THEN 'SI'
+		ELSE 'NO'
+	END) AS 'esJefe'
+FROM DEPOSITO DO
+	JOIN DEPARTAMENTO DA ON DA.depa_zona = DO.depo_zona
+	JOIN EMPLEADO E ON E.empl_departamento = DA.depa_codigo
+GROUP BY DO.depo_codigo
